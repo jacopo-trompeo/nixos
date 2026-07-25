afdd70ed138674e299c5f006adeb539f43b61caa
Jacopo Trompeo <jacopo.trompeo@gmail.com>
2026-07-25 13:55:49 +0200

    fix: new tab page shows custom url

--- system/modules/brave.nix
+++ system/modules/brave.nix
@@ -3,7 +3,7 @@
 {
   environment.etc."brave/policies/managed/nixos.json".text = builtins.toJSON {
     HomepageLocation = "file:///home/jacopo/.config/BraveSoftware/startpage/index.html";
-    HomepageIsNewTabPage = true;
+    HomepageIsNewTabPage = false;
     ShowHomeButton = false;
     RestoreOnStartup = 4;
     RestoreOnStartupURLs = [ "file:///home/jacopo/.config/BraveSoftware/startpage/index.html" ];
